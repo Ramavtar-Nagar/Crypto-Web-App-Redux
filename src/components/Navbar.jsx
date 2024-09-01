bcf3ef0ff573b4dcc954c5171aedce9338250755
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-09-01 05:41:50 +0530

    Development Ready to Go

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
