88b8ab55093cca61c0014cb4f1900d7ef39eaca7
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-09-15 19:23:21 +0530

    Homepage.jsx in Progress

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
+import Loader from './Loader';
+
+const { Title } = Typography;
+
+const Homepage = () => {
+  const { data, isFetching } = useGetCryptosQuery(10);
+  const globalStats = data?.data?.stats;
+
+  if (isFetching) return <Loader />;
+
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default Homepage;
