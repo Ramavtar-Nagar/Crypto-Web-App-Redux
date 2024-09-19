aaf2d93b250bb2f752e0f2c6a2b80c8ae8fb3570
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-09-19 20:45:41 +0530

    News.jsx started

---
+++ src/components/News.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
+
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import Loader from './Loader';
+
+const News = () => {
+  return (
+    <div>News</div>
+  )
+}
+
+export default News
