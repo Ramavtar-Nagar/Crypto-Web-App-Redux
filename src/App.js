add5a826a5ed4df45fe73e51feba4a0cd153105a
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-09-19 20:43:41 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,27 @@ const App = () => {
   return (
     <div className="app">
         <div className="navbar">
-
+          <Navbar />
         </div>
         <div className="main">
-
+          <Layout>
+            <div className="routes">
+              <Switch>
+                <Route exact path="/">
+                  <Homepage />
+                </Route>
+                <Route exact path="/exchanges">
+                  <Exchanges />
+                </Route>
+                <Route exact path="/cryptocurrencies">
+                  <Cryptocurrencies />
+                </Route>
+                <Route exact path="/crypto/:coinId">
+                  <CryptoDetails />
+                </Route>
+              </Switch>
+            </div>
+          </Layout>
         </div>
         <div className="footer">
 
