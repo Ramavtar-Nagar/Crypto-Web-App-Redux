629d913a1079e3ac83e22ec81381f53e1298919f
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-09-20 21:01:35 +0530

    News.jsx in Progress

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -6,7 +6,19 @@ import { useGetCryptosQuery } from '../services/cryptoApi';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 import Loader from './Loader';
 
-const News = () => {
+const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+  const { data } = useGetCryptosQuery(100);
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
+
+  if (!cryptoNews?.value) return <Loader />;
+
+  
   return (
     <div>News</div>
   )
