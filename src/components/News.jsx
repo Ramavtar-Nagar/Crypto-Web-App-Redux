cf6db1af22361065dae9af5b4c62af6a590b971e
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-09-20 23:05:06 +0530

    News.jsx in Progress

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -18,10 +18,26 @@ const News = ({ simplified }) => {
 
   if (!cryptoNews?.value) return <Loader />;
 
-  
-  return (
-    <div>News</div>
-  )
-}
 
-export default News
+  return (
+    <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+          >
+            <Option value="Cryptocurency">Cryptocurrency</Option>
+            {data?.data?.coins?.map((currency) => <Option value={currency.name}>{currency.name}</Option>)}
+          </Select>
+        </Col>
+      )}
+    </Row>
+  );
+};
+
+export default News;
