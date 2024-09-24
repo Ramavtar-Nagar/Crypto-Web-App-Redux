9693eb5f5fc0f66579932f4c211d787aa7152fab
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-09-24 21:11:08 +0530

    App.js Completed

--- src/App.js
+++ src/App.js
@@ -30,11 +30,21 @@ const App = () => (
               </Switch>
             </div>
           </Layout>
-        </div>
-        <div className="footer">
-
+          <div className="footer">
+            <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+              <Link to="/">
+                Cryptoverse Inc.
+              </Link> <br />
+              All Rights Reserved.
+            </Typography.Title>
+            <Space>
+              <Link to="/">Home</Link>
+              <Link to="/exchanges">Exchanges</Link>
+              <Link to="/news">News</Link>
+            </Space>
+            </div>
         </div>
     </div>
   );
 
-export default App
+export default App;
