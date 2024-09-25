86d3d21b4150fcfae5c3ae58357a4212eeafbdb6
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-09-25 22:42:31 +0530

    App.js Updated

--- src/App.js
+++ src/App.js
@@ -38,9 +38,7 @@ const App = () => (
               All Rights Reserved.
             </Typography.Title>
             <Space>
-              <Link to="/">Home</Link>
-              <Link to="/exchanges">Exchanges</Link>
-              <Link to="/news">News</Link>
+              
             </Space>
             </div>
         </div>
